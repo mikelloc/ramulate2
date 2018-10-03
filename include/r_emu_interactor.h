a8ebfc20bfad1c59ce84b7fc159d18363bebb7f1
MiKi <miki@mikelloc.com>
2018-10-03 03:09:14 +0200

    helper function to play wave file using SDL

--- include/r_emu_interactor.h
+++ include/r_emu_interactor.h
@@ -35,10 +35,47 @@ typedef struct r_emu_interactor_plugin_t {
 	void (*init_screen)(REmuInteractor *ei, ut16 width, ut16 height, bool fullscreen);
 	void (*set_pixel)(REmuInteractor *ei, ut16 x, ut16 y, ut32 rgb);
 	void (*init_sound)(REmuInteractor *ei, ut8 voices);
-//	void (*play_wave)(ut8 voice, ...)	//what to do here
+	//	void (*play_wave)(ut8 voice, ...)	//what to do here
 } REmuInteractorPlugin;
 
 
+/*
+ * For playing wave with SDL we need the followig function
+ *
+ * maybe should have a second parameter specifying the playing time
+ *
+ * */
+int play_wave(chat * filename)
+{
+	/*
+	 * Init SDL audo engine and variables
+	 */
+	SDL_Init(SDL_INIT_AUDIO);
+	SDL_AudioSpec wav_desc;
+	uint32 wav_length;
+	uint8 * wav_buffer;
+	/*
+	 * Load file and open audio device
+	 */
+	SDL_LoadWAV(filename, &wav_desc, &wav_buffer, &wav_length);
+	SDL_AudioDeviceID did = SDL_OpenAudioDevice(NULL, 0, &wav_desc, NULL, 0);
+	int success = SDL_QueueAudio(did, wav_buffer, wav_length);
+	/*
+	 * release the pause (so play)
+	 */
+	SDL_PauseAudioDevice(did, 0);
+	/*
+	 * we need to last sufficient time to play sound before close it!
+	 * maybe with:
+	 *	SDL_Delay(milliseconds);
+	 *
+	 * also the cleanup can be done in other separe function
+	 * stop_wave() clean_wave() ?
+	 */
+	SDL_CloseAudioDevice(did);
+	SDL_FreeWAV(wav_buffer);
+	return success;
+}
 
 R_API REmuInteractorPlugin *r_emu_interactor_plugin_get(char *name);
 R_API void *r_emu_interactor_plugin_init(REmuInteractorPlugin *eip);
